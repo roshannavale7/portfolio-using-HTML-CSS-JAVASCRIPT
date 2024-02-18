c70e955d950ad361f0ef0c1b61f587c3cd4ac166
Roshan <roshannavale7@gmail.com>
2024-02-18 14:08:09 +0530

    add more responsiveness

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ window.onscroll = () => {
 //<<<<<<<<<<<<<<<<<<< sticky header >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 let header=document.querySelector('header');
 header.classList.toggle('sticky',window.scrollY >100)
+
+// <<<<<<<<<<<<<<<<<toggle menubar >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function showMenu(){
+    var x = document.getElementById("header")
+    if (x.className==="header"){
+        x.classList.add("active")
+    }
+
+}
+
+
+//<<<<<<<<<<<<<<<<<<< Active class >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+const navLinkActive=document.querySelectorAll(".nav-link");
+
+navLinkActive.forEach(navLinkActive=>{
+    navLinkActive.addEventListener('click',()=>{
+        document.querySelector('.active')?.classList.remove('.active');
+        navLinkActive.classList.add('active')
+    })
+})
